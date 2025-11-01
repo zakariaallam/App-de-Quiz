f8161387731d3df8be419b9a42ed647966cd0904
zakaria allam <zakariaallam536@gmail.com>
2025-11-01 20:14:43 +0100

    modification pour App Quiz

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
 const start_btn = document.getElementById("start_btn");
 const page_start = document.getElementById("page_start");
 const page_Quiz = document.getElementById("page_Quiz");
-let btn_suivent = document.getElementById("btn_suivent");
+
 
 // ------------------- create element -----------------------
 let p = document.createElement("p");
 let button = document.createElement("button");
+let button_presedont = document.createElement("button");
+const div_btn = document.createElement("div")
 
 // ------------- tailwind --------------------
 button.classList.add('px-10', 'cursor-pointer', 'mt-4', 'hover:bg-blue-700', 'py-2', 'rounded-md', 'text-white', 'bg-blue-500');
 
+button_presedont.classList.add('px-10', 'cursor-pointer','hidden', 'mt-4', 'hover:bg-blue-700', 'py-2', 'rounded-md', 'text-white', 'bg-blue-500')
+
+div_btn.classList.add('flex','justify-around')
+
 p.classList.add('text-black-500' ,'font-bold', 'text-2xl', 'mb-8')
+
+button_presedont.textContent = "presedent"
+div_btn.appendChild(button_presedont)
+
+button.textContent = 'Suivent'
+div_btn.appendChild(button)
+
 
 let nuber_question = 0;
 
@@ -44,6 +57,9 @@ const Quize = [
     reponse : "Number()"
   }
 ]
+
+
+let array_scor = [];
 let cunt =0;
 function check_repons(btn){
    for(let i=0;i<Quize.length;i++){
@@ -53,15 +69,15 @@ function check_repons(btn){
     }
    }
 
+
 // -------------- function add element --------------
 function change_quistion(indix){
     p.textContent = `${Quize[indix].Quistion}`
   page_Quiz.appendChild(p)
-  button.textContent = 'Suivent'
   for(let i=0;i<4;i++){
     let button_repons = document.createElement("button");
 
-    button_repons.classList.add('border', 'mb-2', 'm-auto', 'border-gray-400', 'w-180','rounded-xl', 'text-lg', 'px-6', 'py-2')
+    button_repons.classList.add('border', 'mb-2',  'border-gray-400', 'w-180','rounded-xl', 'text-lg', 'px-6', 'py-2')
 
     button_repons.textContent = `${Quize[indix].rp[i]}`
     page_Quiz.appendChild(button_repons)
@@ -77,15 +93,10 @@ function change_quistion(indix){
       check_repons(button_repons)
     })
   }
-    page_Quiz.appendChild(button);
+    page_Quiz.appendChild(div_btn);
 }
 
 
-// function check_corect(btn){
-//   btn.addEventListener("click",()=>{
-
-//   })
-// }
 
 function Add_element(){
   start_btn.addEventListener("click", () => {
@@ -107,22 +118,50 @@ function scor(){
   image.src = './image/congratulation.jpg'
   image.alt = 'image congratulation'
   image.classList.add('m-auto')
-  title.textContent = `Scor : ${cunt}/5`
+  title.textContent = `Scor : ${cunt}/4`
   title.classList.add('font-bold', "text-xl")
   page_Quiz.appendChild(image)
   page_Quiz.appendChild(title);
 
 }
 
+
 button.addEventListener("click",()=>{
   if(nuber_question < 3){
        clear();
        nuber_question++;
        change_quistion(nuber_question);
+       if(nuber_question == 3){
+        button.textContent = 'Termin'
+       }else{
+        button.textContent = 'Suivent'
+       }
+
+       if(nuber_question > 0){
+        button_presedont.classList.remove('hidden')
+       }
+       array_scor[nuber_question];
   }else{
     clear();
     scor();
   }
     });
+
+
+    button_presedont.addEventListener("click",()=>{
+     if(nuber_question <= 3){
+       clear();
+       nuber_question--;
+       change_quistion(nuber_question);
+       if(nuber_question == 0){
+        button_presedont.classList.add('hidden')
+       }
+       
+      }else{
+       clear();
+       scor();
+     }
+     
+     });
 
 Add_element(nuber_question);
